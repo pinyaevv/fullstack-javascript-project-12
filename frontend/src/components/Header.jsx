d4dc4c2de626efa54c6f9f21a78ee97b1a2b5402
Pinyaev Vladimir <avallakhh@gmail.com>
2025-05-19 19:07:21 +0300

    Fix 1. ChatPage

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,11 @@
 import { Container, Navbar, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { logout } from '../app/features/auth/authSlice.js';
 
 const Header = () => {
   const dispatch = useDispatch();
-  const { username } = useSelector((state) => state.auth);
   const { t } = useTranslation();
 
   return (
@@ -18,19 +17,15 @@ const Header = () => {
 
         <Navbar.Collapse id="basic-navbar-nav">
           <div className="user-info ms-auto">
-            {username && (
-              <>
-                <p className="username mb-0">{username}</p>
-                <Button
-                  variant="outline-primary"
-                  onClick={() => dispatch(logout())}
-                  className="logout-btn"
-                  aria-label="logout"
-                >
-                  {t('ui_interface.logout')}
-                </Button>
-              </>
-            )}
+            <Button
+              variant="outline-primary"
+              onClick={() => dispatch(logout())}
+              className="logout-btn"
+              aria-label="logout"
+            >
+              {t('ui_interface.logout')}
+            </Button>
+
           </div>
         </Navbar.Collapse>
       </Container>
